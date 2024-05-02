61b1aa2b88519e12cffd3478d48f3637a1fefc06
lakshmiichoudhary <laxmi2252001@gmail.com>
2024-05-02 21:05:36 +0530

    created a server

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 //console.log("hello world")
 
-const fs = require("fs")
+//const fs = require("fs")
 
-fs.writeFileSync("hello.text","hello world")
+//fs.writeFileSync("hello.text","hello world")
+
+// create a server
+
+const http = require("http")
+
+const server = http.createServer((req,res) => {
+        console.log("Lakshmi")
+})
+
+server.listen(4000)
