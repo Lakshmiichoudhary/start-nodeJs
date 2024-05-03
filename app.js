b78e636ee72758eba04593e7e7ed4f7c873035ee
lakshmiichoudhary <laxmi2252001@gmail.com>
2024-05-03 19:06:09 +0530

    implemented routing

--- app.js
+++ app.js
@@ -9,7 +9,22 @@
 const http = require("http")
 
 const server = http.createServer((req,res) => {
-        console.log("Lakshmi")
+
+       if(req.url === "/") {
+              res.writeHead(200,{"content-Type":"text/plain"});
+              res.end("home Page")
+       } else if (req.url === "/about") {
+              res.writeHead(200, {"content-Type": "text.plain"})
+              res.end("this is about page")
+       } else if(req.url === "/node") {
+              res.writeHead(200, {"content-Type": "text.plain"})
+              res.end("this is node page")
+       } else {
+              res.writeHead(404)
+              res.end("page not found")
+       }
 })
 
-server.listen(4000)
+server.listen(4000,() => {
+       console.log("server running at port 4000")
+})
